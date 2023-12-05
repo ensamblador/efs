2d2234ae73e330d886d2bdffd6e8ccc8b61154ce
ensamblador <enrique.rodriguez.garrido@gmail.com>
2023-12-05 00:09:10 -0300

    done

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 import os
-
+import boto3
 import aws_cdk as cdk
 
 from elastic_file_system.elastic_file_system_stack import ElasticFileSystemStack
 
+TAGS = {"app": "generative ai business apps", "customer": "storage-efs"}
+
+region = os.environ.get("AWS_DEFAULT_REGION")
+if not region: region = "us-east-1"
+
+caller = boto3.client('sts').get_caller_identity()
+account_id = caller.get("Account")
+
 
 app = cdk.App()
-ElasticFileSystemStack(app, "ElasticFileSystemStack",
+stk = ElasticFileSystemStack(app, "efs-stack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
@@ -20,9 +28,12 @@ ElasticFileSystemStack(app, "ElasticFileSystemStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    env=cdk.Environment(account=account_id, region=region),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
 
+if TAGS.keys():
+    for k in TAGS.keys():
+        cdk.Tags.of(stk).add(k, TAGS[k])
 app.synth()
--- elastic_file_system/elastic_file_system_stack.py
+++ elastic_file_system/elastic_file_system_stack.py
@@ -1,19 +1,74 @@
+import boto3
 from aws_cdk import (
     # Duration,
+    aws_ec2 as ec2,
     Stack,
-    # aws_sqs as sqs,
+    aws_iam as iam,
+    aws_efs as efs,
+    aws_ssm as ssm,
 )
+
+from aws_cdk.aws_ec2 import SecurityGroup, Port, Peer
+
 from constructs import Construct
 
-class ElasticFileSystemStack(Stack):
 
+class ElasticFileSystemStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        vpc = self.get_vpc()
+        security_group = self.get_security_group()
 
-        # The code that defines your stack goes here
+        #https://ahmadreza.com/2023/02/how-to-construct-ecs-service-with-efs-volume/
+        filesystem = efs.FileSystem(self, "efs", vpc=vpc, security_group=security_group)
+        
+        # https://github.com/aws-samples/aws-cdk-examples/blob/master/typescript/ecs/fargate-service-with-efs/index.ts
+        filesystem.add_to_resource_policy(
+            iam.PolicyStatement(
+                actions=['elasticfilesystem:ClientMount'], 
+                principals=[iam.AnyPrincipal()], 
+                conditions= {"Bool": {'elasticfilesystem:AccessedViaMountTarget': 'true'}}
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ElasticFileSystemQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        access_point = efs.AccessPoint(
+            self,
+            "volume-accesspoint",
+            file_system=filesystem,
+            path="/data",
+            posix_user=efs.PosixUser(gid="1000", uid="1000"),
+            create_acl=efs.Acl(owner_gid="1000", owner_uid="1000", permissions="755"),
+        )
+        self.create_ssm_param("access-point-id", access_point.access_point_id)
+        self.create_ssm_param("file-system-id", filesystem.file_system_id)
+
+
+    def get_vpc(self):
+        vpc_id = self.get_ssm_parameter("vpc-id")
+        vpc = ec2.Vpc.from_lookup(self, "V", vpc_id=vpc_id)
+
+        return vpc
+
+    def get_security_group(self):
+        task_sg_id = self.get_ssm_parameter("task-sg-id")
+        security_group = SecurityGroup.from_security_group_id(
+            self, "SG", security_group_id=task_sg_id
+        )
+        security_group.add_ingress_rule(
+            security_group, connection=Port.tcp(2049), description="Allow NFS"
+        )
+
+        return security_group
+
+    def get_ssm_parameter(self, parameter_name):
+        return boto3.client("ssm").get_parameter(Name=f"/gen-ai-apps/{parameter_name}")[
+            "Parameter"
+        ]["Value"]
+
+    def create_ssm_param(self, name, value):
+        ssm.StringParameter(
+            self,
+            f"ssm-{name}",
+            parameter_name=f"/gen-ai-apps/{name}",
+            string_value=value,
+        )
